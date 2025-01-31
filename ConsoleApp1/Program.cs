a30cfeb2c6203aead5463b4407b3d22c58b30d41
MaksimLevytskyi <levytskyi.maksim@gmail.com>
2025-01-31 15:20:11 +0200

    You have one unreaded message

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("No, you so bad just acknowledge it");
+Console.WriteLine("KYS");Console.WriteLine("KYS");Console.WriteLine("KYS");Console.WriteLine("KYS");Console.WriteLine("KYS");
+Console.WriteLine("KYS");Console.WriteLine("KYS");Console.WriteLine("KYS");Console.WriteLine("KYS");Console.WriteLine("KYS");Console.WriteLine("KYS");Console.WriteLine("KYS");Console.WriteLine("KYS");Console.WriteLine("KYS");

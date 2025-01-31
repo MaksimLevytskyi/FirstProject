f151c14ce7badd534785d51589af5524d00d2f52
Maksym Levytskyi <levytskyi.maksim@gmail.com>
2025-01-31 15:16:51 +0200

    I have editted your file,bitch

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Darling you so bad just acknowledge it");
+Console.WriteLine("No, you so bad just acknowledge it");
